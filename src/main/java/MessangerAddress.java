d0e5604d13c3743ad4d08443cb8bf69b8dce95b3
Ilya <itchuprakov@yandex.ru>
2024-12-07 10:34:43 +0300

    Чехов написал класс MessangerAddress

--- src/main/java/MessangerAddress.java
+++ src/main/java/MessangerAddress.java
@@ -1,2 +1,18 @@
 public class MessangerAddress {
+    private MessangerAuthor sender = new MessangerAuthor();
+    private MessangerAuthor poluchatel = new MessangerAuthor();
+    private String text;
+    private String time;
+    private void setText(String text) {
+        this.text = text;
+    }
+    private void setSender(MessangerAuthor sender) {
+        this.sender = sender;
+    }
+    private void setPoluchatel(MessangerAuthor poluchatel) {
+        this.poluchatel = poluchatel;
+    }
+    private void setTime(String time) {
+        this.time = time;
+    }
 }
