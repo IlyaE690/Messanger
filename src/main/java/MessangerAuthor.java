4135622e58f4d4d5a38c1f0617ff500cab0be75c
Ilya <itchuprakov@yandex.ru>
2024-12-07 09:33:28 +0300

    Чупраков добавил переменные в MessangerAuthor, написал конструктор

--- src/main/java/MessangerAuthor.java
+++ src/main/java/MessangerAuthor.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class MessangerAuthor {
+    private String name;
+    private String userName;
+    private int numberOfPhone;
+    private String password;
+
+    public MessangerAuthor() {}
+    public MessangerAuthor(String name, String userName, int numberOfPhone, String password) {
+        this.name = name;
+        this.userName = userName;
+        this.numberOfPhone = numberOfPhone;
+        this.password = password;
+    }
 }
